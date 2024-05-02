b511edfc29dbabaf6bfd867f07d3ca1b7a112fb9
Shrrgnien <eduardo.limaoliveira.50@gmail.com>
2024-05-02 16:27:32 -0400

    if else

---
+++ constantes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constantes</title>
+</head>
+<body>
+    <?php
+    define('BD_URL', 'endereço_bd_dev');
+    
+
+    define('BD_USUARIO', 'usuario_dev');
+    define('BD_SENHA', 'sennha_dev');
+
+   echo BD_URL. '<br/>';
+   echo BD_USUARIO. '<br/>';
+   echo BD_SENHA. '<br/>';
+
+
+    ?>
+</body>
+</html>
