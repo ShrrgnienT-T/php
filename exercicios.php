d72438f456367c01a545fd964f82227e137443f3
Shrrgnien <eduardo.limaoliveira.50@gmail.com>
2024-05-03 09:43:40 -0400

    switch

---
+++ exercicios.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $idade = 70;
+    $peso = 51;
+
+    if($idade >= 16 && $idade <= 69 && $peso >=50 ){
+        echo "Atende aos requisitos";
+    }
+    Else{
+        echo "Não atende aos requisitos";
+    }
+
+
+    ?>
+</body>
+</html>
